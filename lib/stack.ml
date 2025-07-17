11a58cf2bceb89bd2e4f5a5afcfd221de24b6e58
Aram Brouwer <aramvanderpols05@gmail.com>
2025-07-17 23:29:16 +0200

    ..

--- lib/stack.ml
+++ lib/stack.ml
@@ -14,12 +14,19 @@ let push (stack: 'a t) (value: 'a): unit =
   stack.data <- a';
   stack.size <- new_size
 
+let add (stack: 'a t) (value: 'a): unit =
+  let new_size = stack.size + 1 in
+  let a'= Array.make new_size value in
+  Array.blit stack.data 0 a' 0 stack.size;
+  stack.data <- a';
+  stack.size <- new_size
+
 let pop (stack: 'a t): 'a =
   if (stack.size <= 0) then raise EmptyStack;
   let new_size = stack.size - 1 in
-  let last_elem = Array.get stack.data new_size in
+  let first_elem = Array.get stack.data 0 in
   let a' = Array.make new_size stack.default in
-  Array.blit stack.data 0 a' 0 new_size;
+  Array.blit stack.data 1 a' 0 new_size;
   stack.data <- a';
   stack.size <- new_size;
-  last_elem
+  first_elem
